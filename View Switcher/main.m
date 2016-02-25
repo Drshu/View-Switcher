dfbf2428028e4aa1446640c5dde07f0398a862e1
Shucheng <741932183@qq.com>
2016-02-25 14:51:02 +0800

    Initial Commit

---
+++ View Switcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  View Switcher
+//
+//  Created by shucheng on 16/2/25.
+//  Copyright © 2016年 shucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
